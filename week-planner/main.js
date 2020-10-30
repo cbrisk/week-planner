ed32dbf910570a0612b072fcc1644477df9102d1
Mrkalen <mrkalen@hotmail.com>
2020-10-30 13:23:52 -0700

    main .js

--- week-planner/main.js
+++ week-planner/main.js
@@ -1,6 +1,24 @@
-var modal = document.querySelector(".add-entry");
-var background = document.query
-document.addEventListener("click", function (event) {
-  if (event.target.matches(modal)) {
+var data = {
+  sunday: [],
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  saturday: []
+}
+
+var modal = document.querySelector('.add-entry');
+var background = document.querySelector('.background');
+
+document.addEventListener('click', function (event) {
+  if (event.target.className === 'add-entry') {
+    background.classList.remove('hidden');
   }
 });
+
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  form.elements.
+})
