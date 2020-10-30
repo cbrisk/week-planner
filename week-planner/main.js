1d9c01c830dc71d056d4023db02a27e767cc99dc
Won Choi <won@Joshs-Macbook.local>
2020-10-30 12:59:27 -0700

    js added

---
+++ week-planner/main.js
@@ -0,0 +1,6 @@
+var modal = document.querySelector(".add-entry");
+var background = document.query
+document.addEventListener("click", function (event) {
+  if (event.target.matches(modal)) {
+  }
+});
