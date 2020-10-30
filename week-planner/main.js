acd277b88ec57df3e7b577069593b974c0459a08
Mrkalen <mrkalen@hotmail.com>
2020-10-30 15:43:40 -0700

    in progress js

--- week-planner/main.js
+++ week-planner/main.js
@@ -1,35 +1,40 @@
-var data = {
-  sunday: [],
-  monday: [],
-  tuesday: [],
-  wednesday: [],
-  thursday: [],
-  friday: [],
-  saturday: [],
-};
+var data = [
+  { sunday: [] },
+  { monday: [] },
+  { tuesday: [] },
+  { wednesday: [] },
+  { thursday: [] },
+  { friday: [] },
+  { saturday: [] }
+];
 
-var modal = document.querySelector(".add-entry");
-var background = document.querySelector(".background");
+var modal = document.querySelector('.add-entry');
+var background = document.querySelector('.background');
+var daybutton = document.querySelectorAll('.day-button');
 
-document.addEventListener("click", function (event) {
-  if (event.target.className === "add-entry") {
-    background.classList.remove("hidden");
+document.addEventListener('click', function (event) {
+  console.log(event.target);
+  if (event.target.className === 'add-entry') {
+    background.classList.remove('hidden');
   }
-  if (event.target.className === 'day-button'){
-    var daybutton = document.querySelectorAll('.day-button');
+  if (event.target.className === 'day-button') {
+    for (var i = 0; i < daybutton.length; i++) {
+      if (event.target === daybutton[i]) {
+        console.log(daybutton[i].textContent);
+      }
+    }
   }
 });
 
-var form = document.querySelector("form");
+var form = document.querySelector('form');
 
-form.addEventListener("submit", function (event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
   var day = form.elements.day.value;
-  data[day].push({
+  data.push(day).push({
     time: form.elements.time.value,
-    description: form.elements.description.value,
+    description: form.elements.description.value
   });
-  background.classList.add("hidden");
+  background.classList.add('hidden');
   form.reset();
 });
-
