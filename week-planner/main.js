ae64bca64eb3cced0f74f16f575751628913b68e
Chaim Brisk <cbrisk99@gmail.com>
2020-11-01 13:35:19 -0800

    added loads

--- week-planner/main.js
+++ week-planner/main.js
@@ -13,7 +13,7 @@ var daybutton = document.querySelectorAll('.day-button');
 var $tablerow = document.querySelectorAll('.table-row');
 var $header = document.querySelector('.header');
 
-var $update = document.createElement('td');
+/*var $update = document.createElement('td');
 $update.setAttribute('class', 'narrow');
 var $updateButton = document.createElement('button');
 $updateButton.setAttribute('class', 'update');
@@ -25,30 +25,40 @@ $delete.setAttribute('class', 'narrow');
 var $deleteButton = document.createElement('button');
 $deleteButton.setAttribute('class', 'delete');
 $deleteButton.textContent = 'Delete';
-$delete.appendChild($deleteButton);
+$delete.appendChild($deleteButton);*/
 
+var $day = null;
 document.addEventListener('click', function (event) {
   if (event.target.className === 'add-entry') {
     background.classList.remove('hidden');
-  }
-  if (event.target.className === 'day-button') {
+  } else if (event.target.className === 'day-button') {
+    $day = event.target.textContent;
     for (var i = 0; i < daybutton.length; i++) {
       if (event.target === daybutton[i]) {
         $header.textContent = 'Scheduled for day of ' + event.target.textContent;
         for (var l = 0; l < $tablerow.length; l++) {
           $tablerow[l].children[0].textContent = '';
           $tablerow[l].children[1].textContent = '';
-          $tablerow[l].removeChild($update);
-          $tablerow[l].removeChild($delete);
+          $tablerow[l].children[2].classList.add('hidden');
+          $tablerow[l].children[3].classList.add('hidden');
         }
         for (var j = 0; j < data[i].entries.length; j++) {
           $tablerow[j].children[0].textContent = data[i].entries[j].time;
           $tablerow[j].children[1].textContent = data[i].entries[j].description;
-          $tablerow[j].appendChild($update);
-          $tablerow[j].appendChild($delete);
+          $tablerow[j].children[2].classList.remove('hidden');
+          $tablerow[j].children[3].classList.remove('hidden');
         }
       }
     }
+  } else if (event.target.className === 'update') {
+    background.classList.remove('hidden');
+    var $parent = event.target.closest('tr');
+    form.elements.day.value = $day;
+    form.elements.time.value = $parent.children[0].textContent;
+    form.elements.description.value = $parent.children[1].textContent;
+    $parent.remove();
+  } else if (event.target.className === 'delete') {
+
   }
 });
 
@@ -72,13 +82,13 @@ form.addEventListener('submit', function (event) {
   for (var l = 0; l < $tablerow.length; l++) {
     $tablerow[l].children[0].textContent = '';
     $tablerow[l].children[1].textContent = '';
-    $tablerow[l].removeChild($update);
-    $tablerow[l].removeChild($delete);
+    $tablerow[l].children[2].classList.add('hidden');
+    $tablerow[l].children[3].classList.add('hidden');
   }
   for (var k = 0; k < data[1].entries.length; k++) {
     $tablerow[k].children[0].textContent = data[1].entries[k].time;
     $tablerow[k].children[1].textContent = data[1].entries[k].description;
-    $tablerow[k].appendChild($update);
-    $tablerow[k].appendChild($delete);
+    $tablerow[k].children[2].classList.remove('hidden');
+    $tablerow[k].children[3].classList.remove('hidden');
   }
 });
